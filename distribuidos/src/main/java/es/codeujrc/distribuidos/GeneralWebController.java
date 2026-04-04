42fc518db599cc31355767781be2a09ebf02ee78
Sergio Espinosa Robles <adriserg2000@gmail.com>
2026-04-04 20:47:24 +0200

    Proyect Correctly First Configurated

---
+++ distribuidos/src/main/java/es/codeujrc/distribuidos/GeneralWebController.java
@@ -0,0 +1,14 @@
+package es.codeujrc.distribuidos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GeneralWebController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+}
