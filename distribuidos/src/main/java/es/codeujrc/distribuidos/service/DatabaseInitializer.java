11245d56fafea92e1bf362ff823fd2af8d487cdb
Pol1705 <pabloleonmolero@gmail.com>
2026-04-09 14:56:56 +0200

    database init i dindt upload r8

--- distribuidos/src/main/java/es/codeujrc/distribuidos/service/DatabaseInitializer.java
+++ distribuidos/src/main/java/es/codeujrc/distribuidos/service/DatabaseInitializer.java
@@ -40,48 +40,28 @@ public class DatabaseInitializer {
 
 	@PostConstruct
 	public void init() throws IOException, URISyntaxException {
+        // Sample users
+        User Carlos = new User(1L, "carlos", passwordEncoder.encode("pass"), "algo@gmail.com", User.Role.USER);
+        User adminuser = new User(2L, "admin", passwordEncoder.encode("adminpass"), "admin@gmail.com", User.Role.ADMIN);
 
-		// Sample Shops
-		Shop shop1 = new Shop("Casa del libro", "C/ Falsa 123");
-		Shop shop2 = new Shop("FNAC", "C/ Falsa 456");
-		Shop shop3 = new Shop("Librería de la plaza", "C/ Falsa 012");
+		userRepository.save(Carlos);
+		userRepository.save(adminuser);
 
-		shopService.save(shop1);
-		shopService.save(shop2);
-		shopService.save(shop3);
+		// Sample decks
+		Deck deck1 = new Deck(1L, "Luffy", "Deck de Luffy", new ArrayList<>(), new ArrayList<>(), Carlos);
+		Deck deck2 = new Deck(2L, "Zoro", "Deck de Zoro", new ArrayList<>(), new ArrayList<>(), adminuser);
+		Deck deck3 = new Deck(3L, "Sanji", "Deck de Sanji", new ArrayList<>(), new ArrayList<>(), adminuser);
 
-		// Sample books
+		deckService.save(deck1);
+		deckService.save(deck2);
+		deckService.save(deck3);
 
-		Book book1 = new Book("SUEÑOS DE ACERO Y NEON",
-				"Los personajes que protagonizan este relato sobreviven en una sociedad en decadencia a la que, no obstante, lograrán devolver la posibilidad de un futuro. Año 2484. En un mundo dominado por las grandes corporaciones, solo un hombre, Jordi Thompson, detective privado deslenguado y vividor, pero de gran talento y sentido d...",
-				new ArrayList<>(Arrays.asList(shop1, shop2)));
+        // Sample cards
+        Card card1 = new Card(1L, "Gomu Gomu no Pistol", "Luffy's signature move", deck1);
+        
 
-		setBookImage(book1, "/sample_images/tus_zonas_erroneas.jpg");
-		bookService.save(book1);
 
-		Book book2 = new Book("LA VIDA SECRETA DE LA MENTE",
-				"La vida secreta de la mentees un viaje especular que recorre el cerebro y el pensamiento: se trata de descubrir nuestra mente para entendernos hasta en los más pequeños rincones que componen lo que somos, cómo forjamos las ideas en los primeros días de vida, cómo damos forma a las decisiones que nos constituyen, cómo soñamos y cómo imaginamos, por qué sentimos ciertas emociones hacia los demás, cómo los demás influyen en nosotros, y cómo el cerebro se transforma y, con él, lo que somos.",
-				new ArrayList<>(Arrays.asList(shop2)));
 
-		setBookImage(book2, "/sample_images/la_vida_secreta_de_la_mente.jpg");
-		bookService.save(book2);
-
-		bookService.save(new Book("CASI SIN QUERER",
-				"El amor algunas veces es tan complicado como impredecible. Pero al final lo que más valoramos son los detalles más simples, los más bonitos, los que llegan sin avisar. Y a la hora de escribir sobre sentimientos, no hay nada más limpio que hacerlo desde el corazón. Y eso hace Defreds en este libro.",
-				new ArrayList<>(Arrays.asList(shop1))));
-
-		bookService.save(new Book("TERMINAMOS Y OTROS POEMAS SIN TERMINAR",
-				"Recopilación de nuevos poemas, textos en prosa y pensamientos del autor. Un sabio dijo una vez: «Pocas cosas hipnotizan tanto en este mundo como una llama y como la luna, será porque no podemos cogerlas o porque nos iluminan en la penumbra». Realmente no sé si alguien dijo esta cita o me la acabo de inventar pero deberían de haberla escrito porque el poder hipnótico que ejercen esa mujer de rojo y esa dama blanca sobre el ser humano es digna de estudio.",
-				new ArrayList<>(Arrays.asList(shop2))));
-
-		bookService.save(new Book("LA LEGIÓN PERDIDA",
-				"En el año 53 a. C. el cónsul Craso cruzó el Éufrates para conquistar Oriente, pero su ejército fue destrozado en Carrhae. Una legión entera cayó prisionera de los partos. Nadie sabe a ciencia cierta qué pasó con aquella legión perdida.150 años después, Trajano está a punto de volver a cruzar el Éufrates. ...",
-				new ArrayList<>(Arrays.asList(shop1, shop2))));
-
-		// Sample users
-
-		userRepository.save(new User(1L, "user", passwordEncoder.encode("pass"), "algo@gmail.com", User.Role.USER));
-		userRepository.save(new User(2L, "admin", passwordEncoder.encode("adminpass"), "admin@gmail.com", User.Role.ADMIN));
 	}
 
 	public void setBookImage(Book book, String classpathResource) throws IOException {
