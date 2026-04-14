28e007494973b4db10246b6ec97df64aa0a9dd72
Pol1705 <pabloleonmolero@gmail.com>
2026-04-14 23:00:33 +0200

    delete decks

--- distribuidos/src/main/java/es/codeujrc/distribuidos/controller/DecksController.java
+++ distribuidos/src/main/java/es/codeujrc/distribuidos/controller/DecksController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.security.Principal;
@@ -67,5 +68,15 @@ public class DecksController {
         model.addAttribute("success", "¡Mazo creado correctamente!");
         model.addAttribute("decks", deckService.findAll());
         return "decks";
+    }
+
+    @PostMapping("/deleteDeck/{id}")
+    public String deleteDeck(@PathVariable Long id, Model model ) {
+
+        deckService.delete(id);
+        
+        model.addAttribute("success", "¡Mazo eliminado correctamente!");
+        model.addAttribute("decks", deckService.findAll());
+        return "redirect:/profile";
     }
 }
