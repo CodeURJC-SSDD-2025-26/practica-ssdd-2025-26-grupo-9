1c8979df759294aea45f401391ed771be414c2c0
Pol1705 <pabloleonmolero@gmail.com>
2026-04-15 18:49:08 +0200

    metacards

--- distribuidos/src/main/java/es/codeujrc/distribuidos/service/CardService.java
+++ distribuidos/src/main/java/es/codeujrc/distribuidos/service/CardService.java
@@ -1,8 +1,10 @@
 package es.codeujrc.distribuidos.service;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,7 +32,7 @@ public class CardService {
 	public void save(Card card) {
 		repository.save(card);
 	}
-	
+
 	public void saveCard(Card card, MultipartFile imageFile) throws IOException {
         if (imageFile != null && !imageFile.isEmpty()) {
             
@@ -50,4 +52,23 @@ public class CardService {
 		}
 		return null;
 	}
+	public Map<String, Object> getMetaCardsData() {
+		
+    // Obtenemos los nombres y el tamaño de su lista de mazos
+    List<Object[]> results = repository.countCardUsageInDecks();
+    
+    List<String> names = new ArrayList<>();
+    List<Integer> deckCounts = new ArrayList<>();
+
+    // Limitamos a las 7 cartas más populares
+    results.stream().limit(7).forEach(row -> {
+        names.add((String) row[0]);
+        deckCounts.add((Integer) row[1]);
+    });
+
+    Map<String, Object> map = new HashMap<>();
+    map.put("names", names);
+    map.put("counts", deckCounts);
+    return map;
+}
 }
